fefc3022b94b68c0712e6825784f30e756a72946
Mathias Javier Murillo <115802009+K1ngHulk@users.noreply.github.com>
2024-10-29 21:37:28 -0500

    Delete src/main/java/pe/upc/learningcenterplatform/learning/service/CreateCourseCommand.java

--- src/main/java/pe/upc/learningcenterplatform/learning/service/CreateCourseCommand.java
+++
@@ -1,4 +0,0 @@
-package pe.upc.learningcenterplatform.courses.domain.model.commands;
-
-public record CreateCourseCommand(String title, String description, String instructor, int duration, String level, String category) {
-}
